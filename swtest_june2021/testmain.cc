038eb3feb8ca7b5e0a5ed7fbf423f22f87c5844c
Tom Junk <trj@fnal.gov>
2021-10-21 12:32:01 -0500

    add switches for writing TPC data out to files or stdout

--- swtest_june2021/testmain.cc
+++ swtest_june2021/testmain.cc
@@ -4,6 +4,9 @@
 #include <string>
 #include <sstream>
 #include <cstring>
+#include <algorithm>
+#include <stdio.h>
+#include "FelixFormat.hh"
 
 std::list<std::string> getTopLevelGroupNames(hid_t fd);
 
@@ -16,7 +19,14 @@ bool attrExists(hid_t object, const char *attrname);
 int main(int argc, char **argv)
 {
 
-  hid_t fd = H5Fopen("swtest_run000333_0000_np04daq_20210608T155720.hdf5",H5F_ACC_RDONLY,H5P_DEFAULT);
+  bool dump_binary_files = false;
+  bool dump_waveforms = false;
+
+  std::string infilename = "/home/trj/dune/app/users/trj/splitter12/work/swt1oct.hdf5";
+  // "sep24swtest.hdf5"
+  // "swtest_run000333_0000_np04daq_20210608T155720.hdf5"
+
+  hid_t fd = H5Fopen(infilename.c_str(),H5F_ACC_RDONLY,H5P_DEFAULT);
   hid_t grp = H5Gopen(fd,"/", H5P_DEFAULT);
   //char name[1000];
   //ssize_t len = H5Iget_name(grp,name,1000);
@@ -65,6 +75,9 @@ int main(int argc, char **argv)
 		    {
 		      std::string dataSetPath = geoGroupPath + "/" + dataSetName;
 		      std::cout << "      Data Set Path: " << dataSetPath << std::endl;
+		      std::string dspu = dataSetPath;
+		      std::replace(dspu.begin(),dspu.end(),'/','_');
+		      std::cout << "      Data Set Path (underscore version): " << dspu << std::endl;
 		      hid_t datasetid = H5Dopen(geoGroup,dataSetName.data(),H5P_DEFAULT);
 		      hsize_t ds_size = H5Dget_storage_size(datasetid);
 		      std::cout << "      Data Set Size (bytes): " << ds_size << std::endl;
@@ -78,13 +91,38 @@ int main(int argc, char **argv)
 		      if (rdr > 0 || narray == 0) narray++;
 		      char *ds_data = new char[narray];
 		      ecode = H5Dread(datasetid, H5T_STD_I8LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, ds_data);
+		      H5Dclose(datasetid);
+
+		      // got all the data for this dataset, can work with it
+
 		      int firstbyte = ds_data[0];
 		      firstbyte &= 0xFF;
 		      int lastbyte = ds_data[narray-1];
 		      lastbyte &= 0xFF;
 		      std::cout << std::hex << "      Retrieved data: ecode: " << ecode << "  first byte: " << firstbyte
 				<< " last byte: " << lastbyte  << std::dec << std::endl;
-                      H5Dclose(datasetid);
+
+                      if (dump_binary_files)
+			{
+			  std::string dspuname = dspu + ".dat";
+		          FILE *ofile = fopen(dspuname.c_str(),"w");
+		          fwrite(ds_data,1,narray,ofile);
+		          fclose(ofile);
+			}
+		      if (dump_waveforms)
+			{
+			  //std::string dspuname = dspu + "_waveforms.txt";
+			  std::cout << dspu << std::endl;
+			  dune::FelixFrame* framedata = reinterpret_cast<dune::FelixFrame*>(ds_data+80);
+			  size_t numframes = (ds_size - 80)/sizeof(dune::FelixFrame);
+			  for (size_t iframe=0;iframe<numframes; ++iframe)
+			    {
+			      for (size_t ichan=0; ichan< 255; ++ichan)
+				{
+				  std::cout << iframe << " " << ichan << " " << framedata[iframe].channel(ichan) << std::endl;
+				}
+			    }
+			}
 
 		      int magic_word = 0;
 		      memcpy(&magic_word,&ds_data[0],4);
@@ -150,11 +188,11 @@ int main(int argc, char **argv)
 		      memcpy(&geoidpadding, &ds_data[76], 4);
 		      //std::cout << "   GeoID padding: " << std::dec << geoidpadding << std::endl;
 
-	              delete[] ds_data;  // free up memory
+		      delete[] ds_data;  // free up memory
 		    }
-                  H5Gclose(geoGroup);
+		  H5Gclose(geoGroup);
 		}
-              H5Gclose(subdetGroup);
+	      H5Gclose(subdetGroup);
 	    }
 	  else if (detectorTypeName == "TriggerRecordHeader")
 	    {
@@ -171,48 +209,48 @@ int main(int argc, char **argv)
 	      if (rdr > 0 || narray == 0) narray++;
 	      char *ds_data = new char[narray];
 	      ecode = H5Dread(datasetid, H5T_STD_I8LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, ds_data);
-		      int firstbyte = ds_data[0];
-		      firstbyte &= 0xFF;
-		      int lastbyte = ds_data[narray-1];
-		      lastbyte &= 0xFF;
-		      std::cout << std::hex << "      Retrieved data: ecode: " << ecode << "  first byte: " << firstbyte
-				<< " last byte: " << lastbyte  << std::dec << std::endl;
-                      H5Dclose(datasetid);
+	      int firstbyte = ds_data[0];
+	      firstbyte &= 0xFF;
+	      int lastbyte = ds_data[narray-1];
+	      lastbyte &= 0xFF;
+	      std::cout << std::hex << "      Retrieved data: ecode: " << ecode << "  first byte: " << firstbyte
+			<< " last byte: " << lastbyte  << std::dec << std::endl;
+	      H5Dclose(datasetid);
 
 
-		      int magic_word = 0;
-		      memcpy(&magic_word,&ds_data[0],4);
-		      std::cout << "   Magic word: 0x" << std::hex << magic_word << std::dec << std::endl;
+	      int magic_word = 0;
+	      memcpy(&magic_word,&ds_data[0],4);
+	      std::cout << "   Magic word: 0x" << std::hex << magic_word << std::dec << std::endl;
 
-		      int version = 0;
-		      memcpy(&version, &ds_data[4],4);
-		      std::cout << "   Version: " << std::dec << version << std::dec << std::endl;
+	      int version = 0;
+	      memcpy(&version, &ds_data[4],4);
+	      std::cout << "   Version: " << std::dec << version << std::dec << std::endl;
 
-		      uint64_t trignum=0;
-		      memcpy(&trignum, &ds_data[8],8);
-		      std::cout << "   Trig Num: " << std::dec << trignum << std::dec << std::endl;
+	      uint64_t trignum=0;
+	      memcpy(&trignum, &ds_data[8],8);
+	      std::cout << "   Trig Num: " << std::dec << trignum << std::dec << std::endl;
 
-		      uint64_t trig_timestamp=0;
-		      memcpy(&trig_timestamp, &ds_data[16],8);
-		      std::cout << "   Trig Timestamp: " << std::dec << trig_timestamp << std::dec << std::endl;
+	      uint64_t trig_timestamp=0;
+	      memcpy(&trig_timestamp, &ds_data[16],8);
+	      std::cout << "   Trig Timestamp: " << std::dec << trig_timestamp << std::dec << std::endl;
 
-		      uint64_t nreq=0;
-		      memcpy(&nreq, &ds_data[24],8);
-		      std::cout << "   No. of requested components:   " << std::dec << nreq << std::dec << std::endl;
+	      uint64_t nreq=0;
+	      memcpy(&nreq, &ds_data[24],8);
+	      std::cout << "   No. of requested components:   " << std::dec << nreq << std::dec << std::endl;
 
-		      int runno=0;
-		      memcpy(&runno, &ds_data[32], 4);
-		      std::cout << "   Run Number: " << std::dec << runno << std::endl;
+	      int runno=0;
+	      memcpy(&runno, &ds_data[32], 4);
+	      std::cout << "   Run Number: " << std::dec << runno << std::endl;
 
-		      int errbits=0;
-		      memcpy(&errbits, &ds_data[36], 4);
-		      std::cout << "   Error bits: " << std::dec << errbits << std::endl;
+	      int errbits=0;
+	      memcpy(&errbits, &ds_data[36], 4);
+	      std::cout << "   Error bits: " << std::dec << errbits << std::endl;
 
-		      short triggertype=0;
-		      memcpy(&triggertype, &ds_data[40], 2);
-		      std::cout << "   Trigger type: " << std::dec << triggertype << std::endl;
+	      short triggertype=0;
+	      memcpy(&triggertype, &ds_data[40], 2);
+	      std::cout << "   Trigger type: " << std::dec << triggertype << std::endl;
 
-	              delete[] ds_data;  // free up memory
+	      delete[] ds_data;  // free up memory
 
 	    }
 	}
