69a55c519e8b9b27d0a10fc6597ab269ac670e39
Tom Junk <trj@fnal.gov>
2022-09-08 09:22:12 -0500

    remove some unneeded flags

--- module0test_sep2022/build.sh
+++ module0test_sep2022/build.sh
@@ -3,4 +3,4 @@ g++ -g -std=c++17 -o tpu tpu.C -I${HDF5_INC} -I${DUNEDETDATAFORMATS_INC} -L${HDF
 
 # from https://svn.ssec.wisc.edu/repos/geoffc/C/HDF5/Examples_by_API/h5ex_t_vlen.c
 
-gcc -g -o tvl tvl.c -I${HDF5_INC} -I${DUNEDETDATAFORMATS_INC} -L${HDF5_FQ_DIR}/lib -lhdf5 -lz -lm
+gcc -g -o tvl tvl.c -I${HDF5_INC} -L${HDF5_FQ_DIR}/lib -lhdf5
